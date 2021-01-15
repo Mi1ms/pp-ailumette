38956860e81769f32cca8bd1ab110af477ad04af
Emilie G <emilie@neo-nomade.com>
2021-01-15 11:31:19 +0100

    playing with IA

---
+++ allumette.js
@@ -0,0 +1,121 @@
+const readline = require('readline-sync');
+const ProgressBar = require('progress');
+const { log } = require('console');
+const { exit } = require('process');
+const { totalmem } = require('os');
+
+const bar = new ProgressBar(':bar', { total: 10 })
+
+
+const play = () => {
+    const luciferTT = 6;
+    const luciferLine = 3;
+    let nbrLucifer = luciferTT;
+    let isUrTurn = true;
+    let theEnd = false;
+    let response
+    // let lucifersArr = fillArr(luciferTT, luciferLine);
+
+    while (!theEnd) {
+        
+        displayLucifers(nbrLucifer, luciferTT);
+        if (isUrTurn) {
+            response = questionUser(nbrLucifer)
+            nbrLucifer = nbrLucifer - response; 
+        } else { 
+            // turn IA
+            response = turnIAPlaying(nbrLucifer);
+            nbrLucifer = nbrLucifer - response; 
+        }
+        console.log(`${isUrTurn ? 'Player' : 'IA'} removed ${response} match${response > 1 ? 'es' : ''} from line`)
+
+        isUrTurn = !isUrTurn;
+        
+        if (nbrLucifer == 0) {
+            theEnd = true;
+        }
+    }
+
+    return isUrTurn ? 'I lost.. snif.. but I’ll get you next time!!' : 'You lost too bad..';
+}
+
+const fillArr = (total, line) => {
+    let resultArr = [];
+    let baseL = total/line;
+    console.log(baseL);
+    let nbrByLine = 1
+
+    for (let index = 0; index < line; index++) {
+        resultArr[index] = [];
+
+        for (let idx = 1; idx >= nbrByLine; idx++) {
+            resultArr[index][idx] = true
+        }
+        nbrByLine++;
+    }
+    console.log(resultArr)
+    exit;
+    return resultArr;
+}
+
+const displayLucifers = (nbr, totalNbr, actualLucifer) => {
+    const lucifer = '|';
+    const border = '*';
+
+    
+    console.log(border.repeat(totalNbr+2));
+    
+    // for (let idx = 1; idx < nbr ; idx++) {
+    //     const luciferLine = lucifer.repeat(idx);
+    //     const space = ' '.repeat((nbr-idx)/2);
+    //     console.log(space + luciferLine + space );
+    //     idx++;
+    // }
+    const luciferLine = lucifer.repeat(nbr);
+    console.log(' '+luciferLine+' ');
+
+    console.log(border.repeat(totalNbr+2));
+}
+
+const questionUser = (totalLucifer) => {
+    console.log('Your turn:');
+
+    let answerLine = readline.question('Line : ');
+    let answerMatch = readline.question('Match : ');
+    
+    while (typeof answerMatch !== 'number' 
+          && answerMatch === 1 || answerMatch === 2 
+          || answerMatch === 3 || answerMatch > totalLucifer) {
+            
+            if (answerMatch > totalLucifer ) {
+                console.error('Error number superior of total lucifer');
+            } else {
+                console.error('Error: Shoud give answer beetween 1 to 3');          
+            }
+            answerMatch = readline.question('Match : ');   
+    }
+    return  answerMatch;
+}
+
+const turnIAPlaying = (luciferSize) => {
+    if (luciferSize > 4) {
+        return randomIntFromInterval(1, 3);
+    } else {
+        
+        return luciferSize !== 1 ? luciferSize-1 : 1;
+    }
+}
+
+function randomIntFromInterval(min, max) {
+    // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+  
+// const timer = setInterval(() => {
+//   bar.tick()
+//   if (bar.complete) {
+//     clearInterval(timer)
+//   }
+// }, 100)
+
+console.log(play())
